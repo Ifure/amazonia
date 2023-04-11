7c782623e29119ad3c70aba9e7a8f22c85fcc008
ifure <ifureudoh08@gmail.com>
2023-04-11 12:08:55 +0100

    first push

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <Image
+              src=" https://links.papareact.com/f90"
+              width={150}
+              height={40}
+
+            />
+        </div>
+    </header>
+  )
+}
+
+export default Header
