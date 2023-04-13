9ef62da62d28279d7c794899eb2b0cedd1b7a8f0
ifure <ifureudoh08@gmail.com>
2023-04-13 22:07:52 +0100

    chaanges

---
+++ src/components/Productfeed.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Productfeed = () => {
+  return (
+    <div>Productfeed</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 
+import { Productfeed } from "../components/Productfeed";
 import { Banner } from "../components/Banner";
 import Header from "../components/Header";
 import Head from "next/head";
@@ -12,6 +13,7 @@ export default function Home() {
       <Header/>
       <main className="max-w-screen-2xl mx-auto">
         <Banner/>
+        <Productfeed/>
       </main>
     </div>
   );
