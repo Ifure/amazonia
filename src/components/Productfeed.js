b8f7609ed122d2c8398b8b5e802bd5cbffcce7eb
ifure <ifureudoh08@gmail.com>
2023-04-16 01:37:24 +0100

    progres

--- src/components/Productfeed.js
+++ src/components/Productfeed.js
@@ -1,21 +1,42 @@
-import React from 'react'
-import { Product } from './Product'
+import React from "react";
+import { Product } from "./Product";
 
-export const Productfeed = ({products}) => {
+export const Productfeed = ({ products }) => {
   // console.log({products})
   return (
-    <div>
+    <>
       <h1>Products here ...</h1>
-      {products.map(({title, id,  description, category, image}) => (
-        <Product
-        key={id}
-        id={id}
-        tiitle={title}
-        description={description}
-        image={image}
-        category={category}
-        />
-      )) }
-    </div>
-  )
-}
+      <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m d:-mt-52 mx-auto">
+        {products
+        .slice(0, 4)
+        .map(({ title, id, description, category, image, price }) => (
+          <Product
+            key={id}
+            id={id}
+            tiitle={title}
+            description={description}
+            image={image}
+            category={category}
+            price={price}
+          />
+        ))}
+        <img src="https://links.papareact.com/dyz" alt="" className="md:col-span-full" />
+        <div className="md:col-span-2">
+        {products
+        .slice(4, 5)
+        .map(({ title, id, description, category, image, price }) => (
+          <Product
+            key={id}
+            id={id}
+            tiitle={title}
+            description={description}
+            image={image}
+            category={category}
+            price={price}
+          />
+        ))}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/product.js
+++ src/components/product.js
@@ -1,28 +1,50 @@
-import { StarIcon } from '@heroicons/react/solid'
-import React, { useState } from 'react'
-import Image from 'next/image'
+import { StarIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import Image from "next/image";
+import Currency from "react-currency-formatter";
 
-export const Product = ({id, description, category, title, image}) => {
-  const MIN_RATING = 1
-  const MAX_RATING = 5
-  const[rating] = useState(
-    Math.floor(Math.random() * (MAX_RATING  - MIN_RATING + 1) )+ MIN_RATING  //to generate random number
-  )
 
+export const Product = ({ id, description, category, title, image, price }) => {
+
+
+  const MIN_RATING = 1;
+  const MAX_RATING = 5;
+
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING //to generate random number
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5); //randomize boolean value
   return (
-    <div>
-      <p>{category}</p>
-      <Image  src={image} height={700} width={200} className="object-contain"   alt=''/>
-      <h4>{title}</h4>
+    <div className="relative m-5 bg-white flex flex-col p-10 z-30">
+      <p  className="absolute top-2 right-2 text-xs italic  text-gray-400">{category}</p>
+      <Image
+        src={image}
+        height={200}
+        width={200}
+        // className="object-contain "
+        alt=""
+      />
+      <h4  className="my-3">{title}</h4>
 
-      <div>
+      <div className="flex">
         {Array(rating)
-        .fill()
-        .map((_, i) => (
-          <StarIcon key={i} className='h-5' />
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5  text-yellow-500" />
           ))}
       </div>
-      
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <div className="mb-5">
+        <Currency quantity={price} currency="USD" />
+      </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img src="https://links.papareact.com/fdw" className="w-12 " alt='' />
+          <p className="text-xs text-gray-500">FREE next-day delivery </p>
+        </div>
+      )}
+      <button className="mt-auto button">Add to Basket</button>
     </div>
-  )
-}
+  );
+};
