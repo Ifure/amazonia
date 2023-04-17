0734cdbe40c42fad60bc5cc08fe70c19c9b530ad
ifure <ifureudoh08@gmail.com>
2023-04-17 12:14:31 +0100

    chNG

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@
 // }
 
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   images: {
     remotePatterns: [
       {
@@ -23,7 +26,5 @@ module.exports = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-},
+  
 }
