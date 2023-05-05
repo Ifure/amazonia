01c72c997f7ebbb0afd99b45eb81a87f104ba821
ifure <ifureudoh08@gmail.com>
2023-05-05 11:13:39 +0100

    changeees

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -10,13 +10,14 @@ const Checkout = () => {
   return (
     <div>
         <Header/>
-        <main className='lg:flex max-w-screen-2xl mx-auto'>
+        <main className='lg:flex  max-w-screen-2xl mx-auto'>
             <div className='flex-grow m-5 shadow-sm'>
                 <Image
                 src='https://links.papareact.com/ikj'
                 width={1020}
                 height={250}
 								alt=''
+								className='w-full'
                 />
                 <div  className='flex flex-col p-5   space-y-10 bg-white '>
 									<h1 className='text-3xl   border-b  pb-4'>
